eca7eff0e4a4cacd00c3dd4e9585f67405f07b13
Rohan140101 <rohan.kuckian@gmail.com>
2020-06-22 19:58:31 +0530

    plot1.png

---
+++ plot2.R
@@ -0,0 +1,19 @@
+library(lubridate)
+
+table1 <- fread("household_power_consumption.txt")
+table1$Date <- dmy(table1$Date)
+table2 <- subset(table1,Date>="2007-02-01" & Date<="2007-02-02")
+table2$Time <- as.ITime(table2$Time)
+
+table2$Global_active_power <- as.numeric(table2$Global_active_power)
+table2$Global_reactive_power <- as.numeric(table2$Global_reactive_power)
+table2$Voltage <- as.numeric(table2$Voltage)
+table2$Global_intensity <- as.numeric(table2$Global_intensity)
+table2$Sub_metering_1 <- as.numeric(table2$Sub_metering_1)
+table2$Sub_metering_2 <- as.numeric(table2$Sub_metering_2)
+
+date_and_time <- strptime(paste(table2$Date,table2$Time),format = "%Y-%m-%d %H:%M:%S")
+
+png("plot2.png", width = 480,height = 480)
+plot(x = date_and_time,y = table2$Global_active_power,xlab = "",ylab = "Global Active Power (kilowatts)",type = "l")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,29 @@
+library(lubridate)
+
+table1 <- fread("household_power_consumption.txt")
+table1$Date <- dmy(table1$Date)
+table2 <- subset(table1,Date>="2007-02-01" & Date<="2007-02-02")
+table2$Time <- as.ITime(table2$Time)
+
+table2$Global_active_power <- as.numeric(table2$Global_active_power)
+table2$Global_reactive_power <- as.numeric(table2$Global_reactive_power)
+table2$Voltage <- as.numeric(table2$Voltage)
+table2$Global_intensity <- as.numeric(table2$Global_intensity)
+table2$Sub_metering_1 <- as.numeric(table2$Sub_metering_1)
+table2$Sub_metering_2 <- as.numeric(table2$Sub_metering_2)
+
+date_and_time <- strptime(paste(table2$Date,table2$Time),format = "%Y-%m-%d %H:%M:%S")
+
+png("plot4.png", width = 480,height = 480)
+
+plot(x = date_and_time,y = table2$Global_active_power,xlab = "",ylab = "Global Active Power (kilowatts)",type = "l")
+
+plot(x = date_and_time,y = table2$Voltage,xlab = "datetime",ylab = "Voltage",type = "l")
+
+plot(x = date_and_time,y = table2$Sub_metering_1,xlab = "",ylab = "Energy Sub Metering",type = "l")
+lines(x = date_and_time,y = table2$Sub_metering_2,col = "red")
+lines(x = date_and_time,y = table2$Sub_metering_3,col = "blue")
+legend("topright",lty = 1,lwd = 1,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col = c("black","red","blue"))
+
+plot(x = date_and_time,y = table2$Global_reactive_power,xlab = "datetime",ylab = "Global Reactive Power",type = "l")
+dev.off()
